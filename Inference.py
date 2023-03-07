09ce34ad87b0bfb3662431c80828bfc0c7d241e5
Niloofarhp <niloofar.hp@gmail.com>
2023-03-07 18:36:01 -0500

    final changes

--- Inference.py
+++ Inference.py
@@ -3,6 +3,7 @@ from deepface import DeepFace
 import os
 import cv2
 import glob
+import shutil
 # it is a simple inference from one image that shows each face detected 
 # and tries to find the similar images from the dataset directory 
 # the dataset directory contains one folder for each face recognized and validated plus one unknown folder
@@ -12,7 +13,7 @@ unknown_path = cwd+"/datasets/unknown"
 tmpImage = 'tmp.jpg'
 datasetFold = cwd+"/datasets/"
 
-def image_is_unknown(facial_img_path):
+def image_is_unknown(facial_img_path, facename):
     for faceFold in os.listdir(datasetFold):
         if faceFold == 'unknown':
             continue
@@ -21,17 +22,19 @@ def image_is_unknown(facial_img_path):
             if(obj['distance'] < 0.5):
                 img = cv2.imread(facial_img_path)
                 count = len(os.listdir(datasetFold+faceFold))
-                cv2.imwrite(datasetFold+faceFold+"/"+faceFold+str(count+1)+".jpg",img)
-                return False
+                facename = faceFold
+                cv2.imwrite(datasetFold+faceFold+"/"+facename+'_'+str(count+1)+".jpg",img)
+                return False,facename
             print(obj['distance'])
-    return True
+    return True,facename
 
-fourcc = cv2.VideoWriter_fourcc(*'H264')
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+# read from input folder the frames stored
+input_path = cwd+"/datasets/input/"
+while len(os.listdir(input_path)) > 0:
+    
+    frame_name = os.listdir(input_path)[0]
+    shutil.move(input_path + os.listdir(input_path)[0], cwd +'/datasets/tmp/'+ frame_name)
+    frame = cv2.imread(cwd +'/datasets/tmp/'+frame_name)
     faces = RetinaFace.detect_faces(frame)
     img = frame 
     for face in faces: 
@@ -40,20 +43,25 @@ while True:
         identity = faces[face]
         facial_area = identity["facial_area"]
         landmarks = identity["landmarks"] 
-        cv2.rectangle(img, (facial_area[2], facial_area[3])
-        , (facial_area[0], facial_area[1]), (255, 255, 255), 1)
+
         
         facial_img = img[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]
         cv2.imwrite(tmpImage,facial_img)
-        if(image_is_unknown(tmpImage)):
+        facename = 'unknown'
+        is_unknown, facename = image_is_unknown(tmpImage,facename)
+        if(is_unknown):
             count = len(os.listdir(unknown_path))
-            cv2.imwrite(unknown_path+'/unknown'+str(count+1)+'.jpg',facial_img)
+            cv2.imwrite(unknown_path+'/'+frame_name +'.jpg',facial_img)
+        cv2.rectangle(img, (facial_area[2], facial_area[3])
+        , (facial_area[0], facial_area[1]), (255, 255, 255), 1)
+        cv2.putText(img, facename, (facial_area[2], facial_area[3]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
 
-    #     cv2.circle(img, (int(landmarks["left_eye"][0]),int(landmarks["left_eye"][1])), 1, (0, 0, 255), -1)
-    #     cv2.circle(img, (int(landmarks["right_eye"][0]),int(landmarks["right_eye"][1])), 1, (0, 0, 255), -1)
-    #     cv2.circle(img, (int(landmarks["nose"][0]),int(landmarks["nose"][1])), 1, (0, 0, 255), -1)
-    #     cv2.circle(img, (int(landmarks["mouth_left"][0]),int(landmarks["mouth_left"][1])), 1, (0, 0, 255), -1)
-    #     cv2.circle(img, (int(landmarks["mouth_right"][0]),int(landmarks["mouth_right"][1])), 1, (0, 0, 255), -1)
+        cv2.circle(img, (int(landmarks["left_eye"][0]),int(landmarks["left_eye"][1])), 1, (0, 0, 255), -1)
+        cv2.circle(img, (int(landmarks["right_eye"][0]),int(landmarks["right_eye"][1])), 1, (0, 0, 255), -1)
+        cv2.circle(img, (int(landmarks["nose"][0]),int(landmarks["nose"][1])), 1, (0, 0, 255), -1)
+        cv2.circle(img, (int(landmarks["mouth_left"][0]),int(landmarks["mouth_left"][1])), 1, (0, 0, 255), -1)
+        cv2.circle(img, (int(landmarks["mouth_right"][0]),int(landmarks["mouth_right"][1])), 1, (0, 0, 255), -1)
+        cv2.imwrite(cwd +'/datasets/output/'+frame_name,img)
     #     cv2.imshow('image preview', img)
     #     cv2.waitKey(0)
     # cv2.destroyAllWindows()
